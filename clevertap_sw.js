7af6a5375a7a8e2367d66b3286a181950cad625d
HARSH SHAH <harshshah@MacBook-Air-3.local>
2018-05-28 13:45:28 +0530

    cool

--- clevertap_sw.js
+++ clevertap_sw.js
@@ -1,3 +1,1 @@
-importScripts("https://harsh093.pushengage.com/service-worker.js?ver=2.0.0");
-importScripts('https://d2r1yp2w7bby2u.cloudfront.net/js/sw_webpush.js');
 importScripts("//cdn.moengage.com/webpush/releases/serviceworker_cdn.min.latest.js?date="+new Date().getUTCFullYear()+""+new Date().getUTCMonth()+""+new Date().getUTCDate());
