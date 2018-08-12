7a8de664bb3f7fac087a62cece17482f7b894bdc
HARSH SHAH <harshshah@MacBook-Air-6.local>
2018-08-12 14:37:52 +0530

    ct migrate only

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,2 @@
 importScripts('https://cdn.izooto.com/scripts/workers/c055429ae1692b709fd942f263e71740bfcd7c09.js');
+importScripts('https://s3-eu-west-1.amazonaws.com/static.wizrocket.com/js/sw_webpush_only_registration.js');
