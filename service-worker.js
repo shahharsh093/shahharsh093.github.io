11991382ef02d649e828b65c62d580320dbf8349
HARSH SHAH <harshshah@MacBook-Air-6.local>
2018-08-12 14:27:19 +0530

    izooto sw

---
+++ service-worker.js
@@ -0,0 +1,1 @@
+importScripts('https://cdn.izooto.com/scripts/workers/c055429ae1692b709fd942f263e71740bfcd7c09.js');
--- serviceworker.js
+++
@@ -1,3 +0,0 @@
-importScripts('https://s3-eu-west-1.amazonaws.com/static.wizrocket.com/js/sw_webpush_only_registration.js');
-importScripts('https://cdn.izooto.com/scripts/workers/c055429ae1692b709fd942f263e71740bfcd7c09.js');
-importScripts("//cdn.moengage.com/webpush/releases/serviceworker_cdn.min.latest.js?date="+new Date().getUTCFullYear()+""+new Date().getUTCMonth()+""+new Date().getUTCDate());
