06a5ecaf5e717b6dd16360a670ba8324094d5a7e
shahharsh093 <shah.harsh093@gmail.com>
2018-09-08 11:28:12 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,1 @@
 importScripts('https://s3-eu-west-1.amazonaws.com/static.wizrocket.com/js/sw_webpush.js');
-importScripts('https://cdn.izooto.com/scripts/workers/c055429ae1692b709fd942f263e71740bfcd7c09.js');
-importScripts('https://s3-eu-west-1.amazonaws.com/static.wizrocket.com/js/sw_webpush_only_registration.js');
